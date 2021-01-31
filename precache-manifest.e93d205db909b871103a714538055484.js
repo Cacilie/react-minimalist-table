fef7cf654940d1027e7c5f8e977ff18d301a0154
Cacilie <hi.ed@hotmail.com>
2021-01-30 23:15:49 -0600

    Updates

---
+++ precache-manifest.e93d205db909b871103a714538055484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b884eaac1793a4fcf0f3f043665219",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2994ffdba06d4f787347",
+    "url": "./static/css/main.f699e1f8.chunk.css"
+  },
+  {
+    "revision": "7546bee0bedc97c7bd6a",
+    "url": "./static/js/2.7602d724.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7602d724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2994ffdba06d4f787347",
+    "url": "./static/js/main.58997d2b.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.58997d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc741916b7f4fc7d75e",
+    "url": "./static/js/runtime-main.2936a475.js"
+  }
+]);
