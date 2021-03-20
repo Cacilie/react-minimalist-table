b36c71ca21ef00dd0565acf5640c8a09e4d8dec7
Cacilie <hi.ed@hotmail.com>
2021-03-20 17:18:55 -0600

    Patch

--- dist/CTableHeader.js
+++ dist/CTableHeader.js
@@ -1,14 +1,16 @@
 import React from 'react';
 export default function CTableHeader(props) {
   const HEADERS = props.data[0] ? Object.keys(props.data[0]) : [];
+  const theadStyles = {
+    backgroundColor: props.headerColor || "#f8f9f5"
+  };
+  const trStyle = {
+    color: props.headerTextColor
+  };
   return /*#__PURE__*/React.createElement("thead", {
-    style: {
-      backgroundColor: props.headerColor || "#f8f9f5"
-    }
+    style: theadStyles
   }, /*#__PURE__*/React.createElement("tr", {
-    style: {
-      color: props.headerTextColor
-    }
+    style: trStyle
   }, HEADERS.map((h, i) => {
     return /*#__PURE__*/React.createElement("th", {
       key: i,
